2094cd1a3d29579f8fa4ff236d2b0bf7e392220b
Jiaying <68321745+Jiaying-e@users.noreply.github.com>
2021-02-21 21:07:43 +1000

    Add files via upload

--- tdf/src/app/user.ts
+++ tdf/src/app/user.ts
@@ -1,2 +1,13 @@
 export class User {
+<<<<<<< HEAD
+=======
+    constructor(
+        public name: string,
+        public email: string,
+        public phone: number,
+        public topic: string,
+        public timePreference: string,
+        public subscribe: boolean
+    ){}
+>>>>>>> 0c9000b (data binding)
 }
